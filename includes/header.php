3970c141fa013ae884b66d87980771ba49f16fbb
Ccbirdy <guochengcheng@live.de>
2021-07-01 01:14:44 +0200

    beautiful icon from fontawsome

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@ require 'config/config.php';
 
 if (isset($_SESSION['username'])) {         /* if you not logged in , you cant see index.php  */
 	$userLoggedIn = $_SESSION['username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username = '$userLoggedIn' ");
+	$user = mysqli_fetch_array($user_details_query);
 }
 else {
 	header("Location: register.php");
@@ -12,9 +14,11 @@ else {
 <html>
 <head>
 	<title>welcome to ccfeed</title>     <!-- https://getbootstrap.com/docs/5.0/getting-started/download/-->
+
 	<!-- Javascript  -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 	<script src="assets/js/bootstrap.js"></script>
+	<script src="https://kit.fontawesome.com/fd2ffeb797.js" crossorigin="anonymous"></script>
 
 
 	<!-- CSS  -->
@@ -30,10 +34,41 @@ else {
 <body>	
 
 	<div class="top_bar">
-		
+
 		<div class="logo">
 			<a href="index.php">CCfeed!</a>		
-			<!--<img src="">		 -->
-			
+			<!--  <img src="">		 -->			
 		</div>
+
+		<nav>
+			  <a href="#">
+					<?php echo $user['first_name'] ?>
+				</a>
+
+				<a href="index.php">
+					<i class="fas fa-igloo"></i> 
+				</a>	
+
+				<a href="#">
+					<i class="far fa-envelope"></i> 
+				</a>
+
+				<a href="#">
+					<i class="fas fa-inbox"></i>
+				</a>	
+
+				<a href="#">
+					<i class="fas fa-user-friends"></i>
+				</a>
+
+				<a href="#">
+					<i class="fas fa-sign-out-alt"></i>
+				</a>
+
+
+		</nav>
+
+
+
+
 	</div>
