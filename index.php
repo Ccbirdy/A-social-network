db865e667ef0fa43b97607b29efcec473f69efcf
Ccbirdy <guochengcheng@live.de>
2021-07-02 02:38:50 +0200

    have sth, to post?

--- index.php
+++ index.php
@@ -6,19 +6,28 @@ include("includes/header.php");
 		<a href="#">  <img src=" <?php echo $user['profile_pic']; ?>">  </a>
 
 		<div class="user_details_left_right"> 
-		<a href="#">  <!-- name is a link to profile -->
+			<a href="#">  <!-- name is a link to profile -->
+			<?php 
+			echo $user['first_name'] . " " .  $user['last_name'];
 
-		<?php 
-		echo $user['first_name'] . " " .  $user['last_name'];
+			 ?>
+			</a>
+			<br>
+			<?php echo "Posts: " . $user['num_posts'] . "<br>" ;
+			echo "Likes: " . $user['num_likes'];
 
-		 ?>
-		</a>
-		<br>
-		<?php echo "Posts: " . $user['num_posts'] . "<br>" ;
-		echo "Likes: " . $user['num_likes'];
+			 ?>
+		 </div>
 
-		 ?>
-
+	</div>
+	<div class="main_column column">
+		<form class="post_form" action="index.php" method="POST">
+			<textarea name="post_text" id="post_text" placeholder="Got something to share?"></textarea>
+			<input type="submit" name="post" id="post_button" value="Post">
+			<hr>
+			
+		</form>
+		
 	</div>
 
 
