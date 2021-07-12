123743a388f183ae2175aa6444f02bb8fc3f2de5
Ccbirdy <guochengcheng@live.de>
2021-07-12 23:49:34 +0200

    i can dinally posst my first post in db

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -1,30 +1,42 @@
 <?php 
 class Post {
-	private $user;
+	private $user_obj;
 	private $con;
 
 	public function __construct($con, $user){
-		$this->con = $con;		
+		$this->con = $con;
 		$this->user_obj = new User($con, $user);
 	}
 
-	public function submitPost($bodym $user_to) {
-		$body = strip_tags($body); // removes html tags
+	public function submitPost($body, $user_to) {
+		$body = strip_tags($body); //removes html tags 
 		$body = mysqli_real_escape_string($this->con, $body);
-		$check_empty = preg_replace('/\s+/','',$body); // delete all spaces
-
-		if ($check_empty != "") {
+		$check_empty = preg_replace('/\s+/', '', $body); // delete all spaces
+ 					  
+		if($check_empty != "") {
 			// if we are in sb else's profile ,allow user to post , like private message?
 			
 			// current date and time
 			$date_added = date("Y-m-d H:i:s");
 			// get username
-			$added_by = $this->$user_obj->getUsername();
+			$added_by = $this->user_obj->getUsername();
 
 			// if user is not on own profile , user_to is 'none'
-			if($user_to = $added_by) {
+			if($user_to == $added_by) {
 				$user_to = "none";
 			}
+
+			// insert post
+			$query = mysqli_query($this->con, "INSERT INTO posts VALUES('', '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+			$returned_id = mysqli_insert_id($this->con);
+
+			// insert notification
+
+			// update post count for user
+			$num_posts = $this->user_obj->getNumPosts();
+			$num_posts++;
+			$update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
+			
 		}
 
 		
--- includes/classes/User.php
+++ includes/classes/User.php
@@ -5,8 +5,19 @@ class User {
 
 	public function __construct($con, $user){
 		$this->con = $con;
-		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username = '$user' ");
+		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user'");
 		$this->user = mysqli_fetch_array($user_details_query);
+	}
+
+	public function getUsername() {
+		return $this->user['username'];
+	}
+
+	public function getNumPosts() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT num_posts FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['num_posts'];
 	}
 
 	public function getFirstAndLastName() {
@@ -17,4 +28,4 @@ class User {
 	}
 }
 
- ?>
+?>
