58f626757305a2842b45a32d89afd4276378fe70
Ccbirdy <guochengcheng@live.de>
2021-07-14 19:29:14 +0200

    57 before infinite scroll

--- index.php
+++ index.php
@@ -37,16 +37,69 @@ if(isset($_POST['post'])){
 			<input type="submit" name="post" id="post_button" value="CCpost !">
 			<hr>
 			
-		</form>
+		</form>		
 
-		<?php 
-
-		$post = new Post($con, $userLoggedIn);
-		$post->loadPostsFriends();
-
-		 ?>
+		<div class="post_area"></div>
+		<img id="#loading" src="assets/images/icons/loading.gif">
 		
 	</div>
+
+	<script>
+	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+
+	$(document).ready(function() {
+
+		$('#loading').show();
+
+		//original ajax request for loading first posts
+		$.ajax({
+			url: "includes/handlers/ajax_load_posts.php",
+			type: "POST",
+			data: "page=1&userLoggedIn=" + userLoggedIn,
+			cache: false,
+
+			success: function(data) {
+				$('#loading').hide();
+				$('.post_area').html(data);
+			}
+		});
+
+		$(window).scroll(function(){
+			var height = $('.post_area').height();// div containing posts
+			var scroll_top = $(this).scrollTop();
+			var page = $('.posts_area').find('.nextPage').val();
+			var noMorePosts = $('post_area').find('.noMorePosts');
+
+			if((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+				$('#loading').show();
+
+
+
+				var ajaxReq = $.ajax({
+					url: "includes/handlers/ajax_load_posts.php",
+					type: "POST",
+					data: "page=" + page + "1&userLoggedIn=" + userLoggedIn,
+					cache: false,
+
+					success: function(response) {
+						$('.post_area').find('.nextPage').remove();  // removes current .nextPage
+						$('.post_area').find('.noMorePosts').remove();  // removes current .noMorePosts
+
+						$('#loading').hide();
+						$('.post_area').append(response);
+					}
+				});
+
+			}// end if
+
+			return false;
+
+		}) //end  $(window).scroll(function()
+
+	});		
+
+
+	</script>
 
 
 	</div>   <!-- closing tag for wrapper in header.php -->
