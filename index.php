99a2ac2ab950b10bb599902a2b0a47beeded15b3
Ccbirdy <guochengcheng@live.de>
2021-07-19 00:28:58 +0200

    checkpoint better infinite scrolling

--- index.php
+++ index.php
@@ -45,59 +45,70 @@ if(isset($_POST['post'])){
 	</div>
 
 	<script>
-	var userLoggedIn = '<?php echo $userLoggedIn; ?>';
-
-	$(document).ready(function() {
-
-		$('#loading').show();
-
-		//original ajax request for loading first posts
-		$.ajax({
-			url: "includes/handlers/ajax_load_posts.php",
-			type: "POST",
-			data: "page=1&userLoggedIn=" + userLoggedIn,
-			cache:false,
-
-			success: function(data) {
-				$('#loading').hide();
-				$('.posts_area').html(data);
-			}
-		});
-
-		$(window).scroll(function() {
-			var height = $('.posts_area').height();// div containing posts
-			var scroll_top = $(this).scrollTop();
-			var page = $('.posts_area').find('.nextPage').val();
-			var noMorePosts = $('.posts_area').find('.noMorePosts').val();
-
-			if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
-				$('#loading').show();			
-
-				var ajaxReq = $.ajax({
-					url: "includes/handlers/ajax_load_posts.php",
-					type: "POST",
-					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-					cache:false,
-
-					success: function(response) {
-						$('.posts_area').find('.nextPage').remove();  // removes current .nextPage
-						$('.posts_area').find('.noMorePosts').remove();  // removes current .noMorePosts
-
-						$('#loading').hide();
-						$('.posts_area').append(response);
-					}
-				});
-
-			}// end if
-
-			return false;
-
-		}); //end  $(window).scroll(function()
-
-	});		
-
-
-	</script>
+   $(function(){
+ 
+       var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+       var inProgress = false;
+ 
+       loadPosts(); //Load first posts
+ 
+       $(window).scroll(function() {
+           var bottomElement = $(".status_post").last();
+           var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+ 
+           // isElementInViewport uses getBoundingClientRect(), which requires the HTML DOM object, not the jQuery object. The jQuery equivalent is using [0] as shown below.
+           if (isElementInView(bottomElement[0]) && noMorePosts == 'false') {
+               loadPosts();
+           }
+       });
+ 
+       function loadPosts() {
+           if(inProgress) { //If it is already in the process of loading some posts, just return
+               return;
+           }
+          
+           inProgress = true;
+           $('#loading').show();
+ 
+           var page = $('.posts_area').find('.nextPage').val() || 1; //If .nextPage couldn't be found, it must not be on the page yet (it must be the first time loading posts), so use the value '1'
+ 
+           $.ajax({
+               url: "includes/handlers/ajax_load_posts.php",
+               type: "POST",
+               data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+               cache:false,
+ 
+               success: function(response) {
+                   $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage
+                   $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage
+                   $('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage
+ 
+                   $('#loading').hide();
+                   $(".posts_area").append(response);
+ 
+                   inProgress = false;
+               }
+           });
+       }
+ 
+       //Check if the element is in view
+       function isElementInView (el) {
+             if(el == null) {
+                return;
+            }
+ 
+           var rect = el.getBoundingClientRect();
+ 
+           return (
+               rect.top >= 0 &&
+               rect.left >= 0 &&
+               rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && //* or $(window).height()
+               rect.right <= (window.innerWidth || document.documentElement.clientWidth) //* or $(window).width()
+           );
+       }
+   });
+ 
+   </script>
 
 
 	</div>   <!-- closing tag for wrapper in header.php -->
