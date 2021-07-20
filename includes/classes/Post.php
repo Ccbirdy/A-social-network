b1ff8391334466308d59d6e91e500dc0db1f98e0
Ccbirdy <guochengcheng@live.de>
2021-07-21 00:56:31 +0200

    like or unlike

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -218,7 +218,7 @@ class Post {
  
 							<div class='newsfeedPostOptions'>
 								Comments($comments_check_num)&nbsp;&nbsp;&nbsp;
-								<!-- <iframe src='like.php?post_id=$id' scrolling='no'></iframe> -->
+								<iframe src='like.php?post_id=$id' scrolling='no'></iframe>
 							</div>
  
 						</div>
--- like.php
+++ like.php
@@ -26,14 +26,38 @@
 		$post_id = $_GET['post_id']; // pass variable to pages name
 	}
 
-	$get_like = mysqli_query($con, "SELECT likes, added_by FROM posts WHERE id='$post_id'");
+	$get_likes = mysqli_query($con, "SELECT likes, added_by FROM posts WHERE id='$post_id'");
 	$row = mysqli_fetch_array($get_likes);
 	$total_likes =$row['likes'];
-	$user_liked = row['added_by'];
-	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='user_liked' ");
+	$user_liked = $row['added_by'];
+
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_liked' ");
 	$row = mysqli_fetch_array($user_details_query);
+	$total_user_likes = $row['num_likes'];
+
+
+
+
 	// like button
+	if(isset($_POST['like_button'])) {
+		$total_likes++;
+		$query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'");
+		$total_user_likes++;
+		$user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+		$insert_user = mysqli_query($con, "INSERT INTO likes VALUES('','$userLoggedIn','$post_id')");
+
+		// insert notification
+	}
+
 	// unlike button
+	if(isset($_POST['unlike_button'])){
+		$total_likes --;
+		$query= mysqli_query($con , "UPDATE posts SET likes = '$total_likes' WHERE id = '$post_id'");
+		$total_user_likes--;
+		$user_likes = mysqli_query($con , "UPDATE users SET num_likes='$total_user_likes' WHERE username = '$user_liked'");
+		$insert_user= mysqli_query($con, "DELETE FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
+    
+	}
 
 
 	// check for previous like
