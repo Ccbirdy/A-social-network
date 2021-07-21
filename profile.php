82d8e28122de3d0bac1e4e2c6ffb8ad3024856bc
Ccbirdy <guochengcheng@live.de>
2021-07-21 18:23:42 +0200

    add friend button

--- profile.php
+++ profile.php
@@ -38,7 +38,30 @@ if(isset($_GET['profile_username'])) {
 	</div>
 
 	<form action="<?php echo $username; ?>" method="POST">
-		$profile_user_obj = new User($con, $username);
+		<?php 
+ 			$profile_user_obj = new User($con, $username); 
+ 			if($profile_user_obj->isClosed()) {
+ 				header("Location: user_closed.php");
+ 			}
+
+ 			$logged_in_user_obj = new User($con, $userLoggedIn); 
+
+ 			if($userLoggedIn != $username) {
+
+ 				if($logged_in_user_obj->isFriend($username)) {
+ 					echo '<input type="submit" name="remove_friend" class="danger" value="Remove Friend"><br>';
+ 				}
+ 				else if ($logged_in_user_obj->didReceiveRequest($username)) {
+ 					echo '<input type="submit" name="respond_request" class="warning" value="Respond to Request"><br>';
+ 				}
+ 				else if ($logged_in_user_obj->didSendRequest($username)) {
+ 					echo '<input type="submit" name="" class="default" value="Request Sent"><br>';
+ 				}
+ 				else 
+ 					echo '<input type="submit" name="add_friend" class="success" value="Add Friend"><br>';
+
+ 			}
+ 			?>
 
 
 	</form>
