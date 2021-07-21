4ed8f00ca2c8d7ff5c9748a30a0b8eeb5f4ee826
Ccbirdy <guochengcheng@live.de>
2021-07-22 01:17:06 +0200

    profile page feed

--- includes/header.php
+++ includes/header.php
@@ -19,6 +19,8 @@ else {
 
 	<!-- Javascript  -->	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 	<!--You are calling the ready function before the jQuery JavaScript is included. Reference jQuery first. -->	
 	<script src="assets/js/bootstrap.js"></script>
 	<script src="assets/js/bootbox.min.js"></script>
