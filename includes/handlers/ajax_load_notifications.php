e70c853aa4b0620bf4ea7ae9abcf759e02a6479b
Ccbirdy <guochengcheng@live.de>
2021-07-24 22:38:24 +0200

    Notification function getNotifications

--- includes/handlers/ajax_load_notifications.php
+++ includes/handlers/ajax_load_notifications.php
@@ -3,6 +3,7 @@ include("../../config/config.php");
 include("../classes/User.php");
 include("../classes/Notification.php");
 
+
 $limit = 7; //Number of messages to load
 
 $notification = new Notification($con, $_REQUEST['userLoggedIn']);
